19f8410bce08eaa55b6f0ca8497f93853f577fe0
IbadUrRehman143 <ibadurrehman010@gmail.com>
2024-08-30 13:10:45 +0500

    o

--- src/Components/PlatForms/PlatForms.jsx
+++ src/Components/PlatForms/PlatForms.jsx
@@ -6,20 +6,20 @@ import { Link } from 'react-router-dom';
 
 const PlatForms = () => {
   return (
-    <div className="tw-bg-GlobalColor05b8906 tw-rounded-tl-full tw-rounded-br-full ">
+    <div className="tw-bg-GlobalColor05b8906  tw-rounded-br-full ">
       <div className="bcontainer">
         <h1 className="md:tw-text-Fxlg tw-text-white tw-text-FLg tw-font-bold tw-text-center tw-py-5">
           Our 5-Star Reviews Across Platforms
         </h1>
         <div className="tw-pb-10 tw-grid tw-gap-5 tw-justify-center md:tw-grid-cols-3 sm:tw-grid-cols-2 tw-grid-cols-1">
           <Link to="https://www.facebook.com/Maazurrahmanofficial/?_rdr">
-            <img src={image2} alt="" className="tw-rounded-2xl tw-w-full md:tw-w-5/6" />
+            <img src={image2} alt="" className="tw-rounded-2xl tw-w-80 md:tw-w-5/6" />
           </Link>
           <Link to="">
-            <img src={image3} alt="" className="tw-rounded-2xl tw-w-full md:tw-w-5/6" />
+            <img src={image3} alt="" className="tw-rounded-2xl tw-w-80 md:tw-w-5/6" />
           </Link>
           <Link to="https://www.google.com/search?gs_ssp=eJwFwUEKgCAQAEC6Bn0hvHTWZQ3LJ_QLdVmRLAsrfH4z_SCjBLjhgNxwV52dVMOF2DPhAkEbzWBVM-R5Ncg-zGQQ3DaGQumMopY3V3F9j8gPiVSzO5x39ANTIxtF&q=coding+souls+pvt+ltd+islamabad&oq=coding+&gs_lcrp=EgZjaHJvbWUqDAgCEC4YJxivARjHATIHCAAQABiPAjIGCAEQRRg7MgwIAhAuGCcYrwEYxwEyCggDEAAYsQMYgAQyBggEEEUYPDIGCAUQRRg8MgYIBhBFGDwyBggHEEUYPNIBCDY1MTNqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8">
-            <img src={image1} alt="" className="tw-rounded-2xl tw-w-full md:tw-w-5/6" />
+            <img src={image1} alt="" className="tw-rounded-2xl tw-w-80 md:tw-w-5/6" />
           </Link>
         </div>
       </div>
--- src/Components/Quote/Quote.jsx
+++ src/Components/Quote/Quote.jsx
@@ -5,7 +5,7 @@ const Quote = () => {
     <div className='tw-bg-GlobalColorc85df0c tw-rounded-tl-full tw-rounded-br-full  tw-mt-36'>
         <div className="bcontainer tw-p-3 tw-py-4">
          <div className='tw-flex tw-items-center md:tw-flex-row tw-flex-col md:tw-justify-between'>
-                <h1>Still Have Question? Or Need A Quote?</h1>
+                <h1  className='tw-text-xl  md:tw-text-5xl'>Still Have Question? Or Need A Quote?</h1>
                 <button className='btn bttn btn-primary'>QUOTE</button>
          </div>
 
