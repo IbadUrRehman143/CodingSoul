918e712273c0e1204867a940cbff97061560a10e
IbadUrRehman143 <ibadurrehman010@gmail.com>
2024-08-30 12:53:18 +0500

    o

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
        <div className="container">
         <div className="row tw-items-center tw-gap-y-8 tw-pt-8">
             <div className="col-12 col-md-6">
-                <h1 className='tw-font-extrabold tw-text-5xl md:tw-text-7xl tw-text-white animate__animated animate__backInLeft'>Coding the Future Today</h1>
-                <p className='tw-text-white tw-text-xl tw-py-3 md:tw-text-2xl animate__animated animate__backInLeft'>Coding the Future Today
+                <h1 className='tw-font-extrabold tw-text-5xl md:tw-text-7xl tw-text-wh animate__animated animate__backInLeft'>Coding the Future Today</h1>
+                <p className='tw-text-whi tw-text-xl tw-py-3 md:tw-text-2xl animate__animated animate__backInLeft'>Coding the Future Today
                 Coding Souls is your trusted coding partner, empowering businesses and individuals to reach new
                  heights since 2020. With cutting-edge solutions and expertise, we bring innovative ideas to life through code.</p>
                  <Link to="#" className='btn tw-bg-GlobalColord4b0483 md:tw-ml-0 animate__animated animate__backInLeft tw-ml-5 tw-mt-9 tw-px-10 tw-text-white tw-text-2xl tw-py-4 tw-font-extrabold hover:tw-bg-GlobalColorc85df0c'>CONTACT</Link>
