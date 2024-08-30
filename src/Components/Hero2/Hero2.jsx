0060f256e34b3b8ea660b3bc5ece77b4f0c2676f
IbadUrRehman143 <ibadurrehman010@gmail.com>
2024-08-31 00:11:00 +0500

    p

--- src/Components/Hero2/Hero2.jsx
+++ src/Components/Hero2/Hero2.jsx
@@ -4,7 +4,7 @@ const Hero2 = ({heading}) => {
   return (
     <>
 
-        <div className="tw-bg-cover  tw-rounded-br-full  tw-h-48" style={{backgroundImage:`url(${back})`}}>
+        <div className="tw-bg-cover tw-bg-amber-200  tw-rounded-br-full  tw-h-48" style={{backgroundImage:`url(${back})`}}>
     <div className="bcontainer">
             <h1 className='tw-text-5xl tw-text-white tw-text-center   tw-py-6'>{heading}</h1>
         </div>
