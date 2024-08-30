01d1af35bc3b4b223398c6e0fa68d5a6f2fa570d
IbadUrRehman143 <ibadurrehman010@gmail.com>
2024-08-30 18:26:45 +0500

    l

--- src/Components/Copywrite/Copywrite.jsx
+++ src/Components/Copywrite/Copywrite.jsx
@@ -7,7 +7,7 @@ const Copywrite = () => {
         <div className='bcontainer tw-text-center '>
           <footer className="tw-footer  tw-text-neutral-content tw-justify-evenly  tw-p-4">
             <aside className="grid-flow-col ">
-              <p className='tw-text-center tw-text-xl '>Copyright © {new Date().getFullYear()} - All right reserved - Coding Souls (Private) Limited</p>
+              <p className='tw-text-center md:tw-text-xl tw-text-sm tw-ml-8 md:tw-ml-0 '>Copyright © {new Date().getFullYear()} - All right reserved - Coding Souls (Private) Limited</p>
             </aside>
           </footer>
         </div>
